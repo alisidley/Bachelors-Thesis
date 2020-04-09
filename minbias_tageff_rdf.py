aea4d39710f3d0de5c4a313e668ea676d6d79f22
Alexandra Sidley <asidley@stbc-i2.nikhef.nl>
2020-04-09 16:25:05 +0200

    slgih

--- minbias_tageff_rdf.py
+++ minbias_tageff_rdf.py
@@ -10,18 +10,16 @@ input_tree_name = "Bs2jpsiphi/DecayTree"
 dataframe = RDataFrame(input_tree_name, input_ntuple)
 cuts = "(Bs_M > 5150) && (Bs_M < 5550) && (Jpsi_M > 3020) && (Jpsi_M < 3170) && (Phi_M > 980) && (Phi_M < 1050) && (muplus_PT > 500) && (mumin_PT > 500) && ((Bs_ENDVERTEX_CHI2/Bs_ENDVERT\
 EX_NDOF) < 20) && (Jpsi_ENDVERTEX_CHI2/Jpsi_ENDVERTEX_NDOF < 16) && (Phi_ENDVERTEX_CHI2/Phi_ENDVERTEX_NDOF < 25)"
-df_cuts = dataframe.Filter(cuts)
+cuts_loose = "(Bs_M > 5150) && (Bs_M < 5550)"
+df_cuts = dataframe.Filter(cuts_loose)
 
-nentries = df_cuts.Count()
+nentries = df_cuts.Count().GetValue()
 print(nentries)
 
-df_right_tag = dataframe.Filter("(Bs_OSMuon_TAGDEC == 1 && Bs_TRUEID == 1) || (Bs_OSMuon_TAGDEC == -1 && Bs_TRUEID == -1)")
-df_wrong_tag = dataframe.Filter("(Bs_OSMuon_TAGDEC == 1 && Bs_TRUEID != 1) || (Bs_OSMuon_TAGDEC == -1 && Bs_TRUEID != -1)")
-df_untagged = dataframe.Filter("Bs_OSMuon_TAGDEC = 0")
+R = df_cuts.Filter("(Bs_OSMuon_TAGDEC == 1 && Bs_TRUEID == 531) || (Bs_OSMuon_TAGDEC == -1 && Bs_TRUEID == -531)").Count().GetValue()
+W = df_cuts.Filter("(Bs_OSMuon_TAGDEC == 1 && Bs_TRUEID != 531) || (Bs_OSMuon_TAGDEC == -1 && Bs_TRUEID != -531)").Count().GetValue()
+U = df_cuts.Filter("Bs_OSMuon_TAGDEC == 0").Count().GetValue()
 
-R = df_right_tag.Count().GetValue()
-W = df_wrong_tag.Count().GetValue()
-U = df_untagged.Count().GetValue()
 print(R)
 print(W)
 print(U)
